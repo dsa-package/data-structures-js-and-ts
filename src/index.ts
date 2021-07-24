8d7fbfe32fd2b2f3aa8f430139404b5aef3a5531
VivinMeth <vivinmeth@gmail.com>
2021-07-24 20:33:36 +0530

    package setup done

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export const text = "Welcome to <package_name> Package. This is a old version of the package upgrade to new version to use it.";
+
+export const DS = () => {
+    console.log(text);
+
+    //return text;
+}
